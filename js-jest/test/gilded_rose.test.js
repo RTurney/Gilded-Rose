759e8ced8e736d801151b0bf562cc722d2a59226
RTurney <r.turney93@gmail.com>
2021-03-10 16:19:39 +0000

    Add test to check items return value

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -3,10 +3,16 @@ const {Shop, Item} = require("../src/gilded_rose");
 describe("Gilded Rose", () => {
 
   describe("#items", () => {
-    it("items return their name", () => {
+    it('items return their name', () => {
       const gildedRose = new Shop([new Item("foo", 0, 0)]);
-      const items = gildedRose.updateQuality();
+      const items = gildedRose.items;
       expect(items[0].name).toBe("foo");
+    });
+
+    it('items return their up-to-date sellin value', () => {
+      const gildedRose = new Shop([new Item("foo", 0, 0)]);
+      const items = gildedRose.items;
+      expect(items[0].sellIn).toEqual(0);
     });
   });
 });
