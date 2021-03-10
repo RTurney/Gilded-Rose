6119c7556e5596a76c108b514b752a94f875f16f
RTurney <r.turney93@gmail.com>
2021-03-10 16:28:34 +0000

    Add test for item quality property

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -11,15 +11,15 @@ describe("Gilded Rose", () => {
     });
 
     it('items return their name', () => {
-      // const gildedRose = new Shop([new Item("foo", 0, 0)]);
-      // const items = gildedRose.items;
       expect(items[0].name).toBe("foo");
     });
 
-    it('items return their up-to-date sellin value', () => {
-      // const gildedRose = new Shop([new Item("foo", 0, 0)]);
-      // const items = gildedRose.items;
+    it('items return their sellIn date', () => {
       expect(items[0].sellIn).toEqual(0);
+    });
+
+    it('items return their quality', () => {
+      expect(items[0].quality).toEqual(0);
     });
   });
 });
