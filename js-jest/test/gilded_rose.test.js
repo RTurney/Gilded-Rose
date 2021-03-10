8f9ff1394879d9bcfd90428f9e750b87dd7808c2
RTurney <r.turney93@gmail.com>
2021-03-10 16:13:52 +0000

    refactor jest test to include items group
    Signed-off-by: RTurney <r.turney93@gmail.com>

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,12 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", () => {
-  it("items return their name", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+
+  describe("#items", () => {
+    it("items return their name", () => {
+      const gildedRose = new Shop([new Item("foo", 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toBe("foo");
+    });
   });
 });
