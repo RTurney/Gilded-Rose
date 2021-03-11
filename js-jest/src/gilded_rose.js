55f385040dc9e35358731fb8bad761ec3777f606
RTurney <r.turney93@gmail.com>
2021-03-11 14:28:21 +0000

    Add reduce sellIn function

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -19,6 +19,10 @@ class Shop {
     item.quality += 1;
   }
 
+  sellInReducer(item) {
+    item.sellIn -= 1;
+  }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -45,7 +49,7 @@ class Shop {
         }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.sellInReducer(this.items[i]);
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
