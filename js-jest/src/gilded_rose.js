d562d376af70cc0acddfe4fcb068e0e9e1d7cbb6
RTurney <r.turney93@gmail.com>
2021-03-11 14:26:09 +0000

    I missed a quality increaser apparently

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -29,7 +29,7 @@ class Shop {
         }
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+            this.qualityIncreaser(this.items[i]);
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
