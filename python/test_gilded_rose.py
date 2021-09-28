bc280dda6ea13cd297f888bce7325f3237038877
RTurney <r.turney93@gmail.com>
2021-09-28 19:25:46 +0100

    Refactor tests with setup function

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -6,29 +6,31 @@ from item import Item
 
 
 class GildedRoseTest(unittest.TestCase):
+    def setUp(self):
+        self.items = [
+            Item("foo", 0, 1),
+            Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            Item("Aged Brie", 0, 0)
+        ]
+        self.gilded_rose = GildedRose(self.items)
+
     def test_foo(self):
-        items = [Item("foo", 0, 1)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("foo", items[0].name)
-        self.assertEqual(-1, items[0].sell_in)
-        self.assertEqual(0, items[0].quality)
+        self.gilded_rose.update_quality()
+        self.assertEqual("foo", self.items[0].name)
+        self.assertEqual(-1, self.items[0].sell_in)
+        self.assertEqual(0, self.items[0].quality)
 
     def test_sulfuras_checker(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("Sulfuras, Hand of Ragnaros", items[0].name)
-        self.assertEqual(0, items[0].sell_in)
-        self.assertEqual(80, items[0].quality)
+        self.gilded_rose.update_quality()
+        self.assertEqual("Sulfuras, Hand of Ragnaros", self.items[1].name)
+        self.assertEqual(0, self.items[1].sell_in)
+        self.assertEqual(80, self.items[1].quality)
 
     def test_aged_brie(self):
-        items = [Item("Aged Brie", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("Aged Brie", items[0].name)
-        self.assertEqual(-1, items[0].sell_in)
-        self.assertEqual(2, items[0].quality)
+        self.gilded_rose.update_quality()
+        self.assertEqual("Aged Brie", self.items[2].name)
+        self.assertEqual(-1, self.items[2].sell_in)
+        self.assertEqual(2, self.items[2].quality)
 
 
 if __name__ == '__main__':
